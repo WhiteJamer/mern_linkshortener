e8916eaecc0072897e1ab63d03bc701432a6a652
whitejamer <mihailkuzin.job@yandex.ru>
2020-04-13 17:26:51 +0300

    add production settings

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const express = require("express");
+const path = require("path");
 const config = require("config");
 const mongoose = require("mongoose");
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", () => {
+    express.static(path.join(__dirname, "client", "build"));
+    app.use("*", (req, res) => {
+      res.sendFile(
+        path.resolve(path.join(__dirname, "client", "build", "index.html"))
+      );
+    });
+  });
+}
 const PORT = config.get("port") || 5000;
 
 const app = express();
 
-app.use(express.json({extended: true})); // Для парсинга JSON
+app.use(express.json({ extended: true })); // Для парсинга JSON
 
-app.use('/r', require('./routes/redirect.routes'));
+app.use("/r", require("./routes/redirect.routes"));
 app.use("/api/links", require("./routes/links.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
-
 
 async function start() {
   try {
     await mongoose.connect(config.get("mongoUrl"), {
       useUnifiedTopology: true,
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
-    console.log('Connect to DB: Success')
+    console.log("Connect to DB: Success");
     app.listen(PORT, () =>
       console.log(`APP has been started on ${PORT} port ...`)
     );
